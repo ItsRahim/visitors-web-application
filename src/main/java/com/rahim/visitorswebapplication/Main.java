4dd070e58afbdbd4b07cf358889ab3d9bfe81b52
ItsRahim <rahim1605@gmail.com>
2022-11-30 12:00:15 +0000

    added commandlinerunner

--- src/main/java/com/rahim/visitorswebapplication/Main.java
+++ src/main/java/com/rahim/visitorswebapplication/Main.java
@@ -1,11 +1,36 @@
 package com.rahim.visitorswebapplication;
 
+import com.rahim.visitorswebapplication.Entity.Employee;
+import com.rahim.visitorswebapplication.Enum.EmployeeRole;
+import com.rahim.visitorswebapplication.Repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class Main {
 
+    @Bean
+    CommandLineRunner addEmployee(EmployeeRepository employeeRepo) {
+        return args -> {
+            Employee rahim = new Employee(
+                    UUID.randomUUID().toString(),
+                    "Rahim",
+                    "Ahmed",
+                    LocalDate.of(2001, Month.AUGUST, 17),
+                    "rahim.ahmed@bupa.com",
+                    LocalDate.of(2019, Month.DECEMBER, 21),
+                    EmployeeRole.HOME_MANAGER
+            );
+            employeeRepo.saveAll(List.of(rahim));
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
