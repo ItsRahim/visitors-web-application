a0260541b666d80f6aeb1d0465b35bd36aa0c178
ItsRahim <rahim1605@gmail.com>
2022-11-30 12:08:16 +0000

    created entity for residents2

---
+++ src/main/java/com/rahim/visitorswebapplication/Entity/Resident.java
@@ -0,0 +1,30 @@
+package com.rahim.visitorswebapplication.Entity;
+
+import com.rahim.visitorswebapplication.Enum.ResidentUnit;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Resident {
+    @Id
+    @Column(unique = true)
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+
+    @Enumerated(EnumType.STRING)
+    private ResidentUnit unit;
+
+    private String fundingType;
+    private int band;
+    private LocalDate admissionDate;
+}
