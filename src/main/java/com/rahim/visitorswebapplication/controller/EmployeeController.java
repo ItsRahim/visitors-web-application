cd69996323e7544374d1f1ab1056bf93734d1a74
ItsRahim <rahim1605@gmail.com>
2022-11-30 13:01:37 +0000

    created service to add new employee with HTTP Status repsonse

--- src/main/java/com/rahim/visitorswebapplication/controller/EmployeeController.java
+++ src/main/java/com/rahim/visitorswebapplication/controller/EmployeeController.java
@@ -3,9 +3,8 @@ package com.rahim.visitorswebapplication.controller;
 import com.rahim.visitorswebapplication.model.Employee;
 import com.rahim.visitorswebapplication.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -16,8 +15,15 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/list")
     public Collection<Employee> listAll() {
         return employeeService.listAll(10);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
+    public Employee create(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
     }
 }
