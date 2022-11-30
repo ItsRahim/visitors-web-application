c95a7893373f75c8b6023c70c80a5083a675b549
ItsRahim <rahim1605@gmail.com>
2022-11-30 11:21:30 +0000

    created enumerations for booking status

--- src/main/java/com/rahim/visitorswebapplication/Enum/BookingStatus.java
+++ src/main/java/com/rahim/visitorswebapplication/Enum/BookingStatus.java
@@ -1,4 +1,14 @@
 package com.rahim.visitorswebapplication.Enum;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum BookingStatus {
+    PENDING,
+    ACTIVE,
+    COMPLETE;
+
+    @Getter
+    public String bookingStatus;
 }
