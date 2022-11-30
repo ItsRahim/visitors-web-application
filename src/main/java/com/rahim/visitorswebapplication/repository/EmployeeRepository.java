0f8373caf4e479de3f3b719f50ee3ebca0e64c0f
ItsRahim <rahim1605@gmail.com>
2022-11-30 12:44:34 +0000

    added default method if employee with id is not found

--- src/main/java/com/rahim/visitorswebapplication/repository/EmployeeRepository.java
+++ src/main/java/com/rahim/visitorswebapplication/repository/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.rahim.visitorswebapplication.repository;
 
 import com.rahim.visitorswebapplication.model.Employee;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
+
+    default Employee findByIdOrError(String id) {
+        return findById(id).orElseThrow(EntityNotFoundException::new);
+    }
 }
