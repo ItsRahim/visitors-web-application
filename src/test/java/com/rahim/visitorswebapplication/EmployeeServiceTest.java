19cfb4fe4c7a7b05ec2f8aecfbea11e3204a2857
ItsRahim <rahim1605@gmail.com>
2022-12-01 16:27:14 +0000

    added unit test to check email formatter

--- src/test/java/com/rahim/visitorswebapplication/EmployeeServiceTest.java
+++ src/test/java/com/rahim/visitorswebapplication/EmployeeServiceTest.java
@@ -1,8 +1,8 @@
 package com.rahim.visitorswebapplication;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rahim.visitorswebapplication.service.EmployeeService;
+import com.rahim.visitorswebapplication.configurators.EmployeeConfigurations;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
@@ -20,20 +21,23 @@ class EmployeeServiceTest {
 
     @Autowired
     private MockMvc mockMvc;
-    private ObjectMapper objectMapper;
-    private EmployeeService employeeService;
     private final String baseUrl = "/api/v2/employee";
 
     @Test
     public void shouldCreateEmployee() throws Exception {
-        mockMvc.perform(post(baseUrl+"/create")
+        mockMvc.perform(post(baseUrl + "/create")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"id\": \"UUID.randomUUID().toString()\",\"firstname\": \"Jasmin\", " +
+                .content("{\"id\": \"UUID.randomUUID().toString()\"," +
+                        "\"firstname\": \"Jasmin\", " +
                         "\"lastname\": \"Khanam\"," +
                         "\"dob\": \"1980-05-31\"," +
                         "\"email\": \"jasmin.khanam@yahoo.co.uk\"," +
                         "\"startDate\": \"2012-07-15\"," +
                         "\"role\": \"ACTIVITIES_COORDINATOR\"}"))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+    @Test
+    public void shouldFormatEmailCorrectly() {
+        assertEquals(EmployeeConfigurations.emailFormatter("jasmin", "khanam"), "jasmin.khanam@bupa.com");
     }
 }
