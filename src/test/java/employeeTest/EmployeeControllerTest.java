9e54744752fc9039275b5aa6c09f9fe236856e70
ItsRahim <rahim1605@gmail.com>
2022-12-20 16:25:24 +0000

    added configuration annotation and created mock service

--- src/test/java/employeeTest/EmployeeControllerTest.java
+++ src/test/java/employeeTest/EmployeeControllerTest.java
@@ -1,5 +1,8 @@
 package employeeTest;
 
+import com.rahim.visitorswebapplication.Main;
+import com.rahim.visitorswebapplication.controller.EmployeeController;
+import com.rahim.visitorswebapplication.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -7,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -17,15 +23,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @SpringBootTest
 @AutoConfigureMockMvc
 @RequiredArgsConstructor
+@ContextConfiguration(classes = Main.class)
 class EmployeeControllerTest {
 
+    EmployeeService employeeService = mock(EmployeeService.class);
+    EmployeeController employeeController = new EmployeeController(employeeService);
+
     @Autowired
-    private MockMvc mockMvc;
-    private final String baseUrl = "/api/v2/employee";
+    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(employeeController).build();
 
     @Test
     void shouldCreateEmployee() throws Exception {
-        mockMvc.perform(post(baseUrl + "/create")
+        mockMvc.perform(post("/api/v2/employee/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"id\": \"UUID.randomUUID().toString()\"," +
                                 "\"firstname\": \"Jasmin\", " +
@@ -39,14 +48,14 @@ class EmployeeControllerTest {
 
     @Test
     void shouldGetAllUsers() throws Exception {
-        mockMvc.perform(get(baseUrl + "/list"))
+        mockMvc.perform(get("/api/v2/employee/list"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(1)));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(2)));
     }
 
     @Test
     void shouldDeleteUser() throws Exception {
-        mockMvc.perform(delete(baseUrl + "delete/{id}", "11"))
+        mockMvc.perform(delete("/api/v2/employee/delete/{id}", "11"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
