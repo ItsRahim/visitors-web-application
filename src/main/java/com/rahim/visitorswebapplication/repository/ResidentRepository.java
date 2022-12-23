f3502b973ee061d55bbc8d5efde4d8eb448c73bf
ItsRahim <rahim1605@gmail.com>
2022-12-23 10:48:11 +0000

    added default method when searching by id

--- src/main/java/com/rahim/visitorswebapplication/repository/ResidentRepository.java
+++ src/main/java/com/rahim/visitorswebapplication/repository/ResidentRepository.java
@@ -1,6 +1,7 @@
 package com.rahim.visitorswebapplication.repository;
 
 import com.rahim.visitorswebapplication.model.Resident;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface ResidentRepository extends JpaRepository<Resident, String> {
 
+    default Resident findByIdOrError(String id) {
+        return findById(id).orElseThrow(EntityNotFoundException::new);
+    }
     @Modifying
     @Transactional
     @Query("DELETE FROM Resident e WHERE e.id = ?1")
