b85675a439ec4cbeefe7dbf2f7023b7cbf0fc041
ItsRahim <rahim1605@gmail.com>
2022-12-23 10:46:41 +0000

    added query to delete resident by id

--- src/main/java/com/rahim/visitorswebapplication/repository/ResidentRepository.java
+++ src/main/java/com/rahim/visitorswebapplication/repository/ResidentRepository.java
@@ -2,8 +2,16 @@ package com.rahim.visitorswebapplication.repository;
 
 import com.rahim.visitorswebapplication.model.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ResidentRepository extends JpaRepository<Resident, String> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Resident e WHERE e.id = ?1")
+    void deleteByResidentId(String Id);
 }
