3ff30fc662a948cc13d2abff87e9bb81bc1000f2
ItsRahim <rahim1605@gmail.com>
2022-12-29 12:47:34 +0000

    implemented model for visitor class

--- src/main/java/com/rahim/visitorswebapplication/model/Visitor.java
+++ src/main/java/com/rahim/visitorswebapplication/model/Visitor.java
@@ -1,4 +1,21 @@
 package com.rahim.visitorswebapplication.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Visitor {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    
 }
