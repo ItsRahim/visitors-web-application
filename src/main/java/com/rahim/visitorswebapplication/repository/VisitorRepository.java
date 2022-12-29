bcefeba796404508b42a47c17be614301b189c8f
ItsRahim <rahim1605@gmail.com>
2022-12-29 15:09:44 +0000

    added default method

--- src/main/java/com/rahim/visitorswebapplication/repository/VisitorRepository.java
+++ src/main/java/com/rahim/visitorswebapplication/repository/VisitorRepository.java
@@ -1,6 +1,7 @@
 package com.rahim.visitorswebapplication.repository;
 
 import com.rahim.visitorswebapplication.model.Visitor;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface VisitorRepository extends JpaRepository<Visitor, String> {
+
+    default Visitor findByIdOrError(String id) {
+        return findById(id).orElseThrow(EntityNotFoundException::new);
+    }
     @Modifying
     @Transactional
     @Query("DELETE FROM Visitor v WHERE v.id = ?1")
