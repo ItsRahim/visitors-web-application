d904f7782eba7827c5b39be909beb4a78859dd4e
ItsRahim <rahim1605@gmail.com>
2023-01-06 13:51:29 +0000

    removed transactional annotation from repo

--- src/main/java/com/rahim/visitorswebapplication/repository/EmployeeRepository.java
+++ src/main/java/com/rahim/visitorswebapplication/repository/EmployeeRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
@@ -16,7 +15,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
     }
 
     @Modifying
-    @Transactional
     @Query("DELETE FROM Employee e WHERE e.id = ?1")
     void deleteByEmployeeId(String Id);
 }
