82436d21a6879a48032064a5829a10d4de87f029
ItsRahim <rahim1605@gmail.com>
2023-01-06 11:09:11 +0000

    created method for booking interface

--- src/main/java/com/rahim/visitorswebapplication/service/BookingService.java
+++ src/main/java/com/rahim/visitorswebapplication/service/BookingService.java
@@ -1,4 +1,14 @@
 package com.rahim.visitorswebapplication.service;
 
+import com.rahim.visitorswebapplication.model.Booking;
+
+import java.util.Collection;
+
 public interface BookingService {
+    Booking createBooking(Booking booking);
+    Booking updateBooking(Booking booking, String id);
+    void deleteBooking(String id);
+    Booking getBooking(String id);
+    void processBooking(String id);
+    Collection<Booking> listAll(int limit);
 }
