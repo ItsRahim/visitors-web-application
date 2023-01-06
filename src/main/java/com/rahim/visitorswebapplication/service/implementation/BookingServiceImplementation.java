9877af68fd7fccf30d97b3a8e4f21f7a85b57954
ItsRahim <rahim1605@gmail.com>
2023-01-06 11:09:30 +0000

    added transactional annotation to class

--- src/main/java/com/rahim/visitorswebapplication/service/implementation/BookingServiceImplementation.java
+++ src/main/java/com/rahim/visitorswebapplication/service/implementation/BookingServiceImplementation.java
@@ -2,8 +2,10 @@ package com.rahim.visitorswebapplication.service.implementation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class BookingServiceImplementation {
 }
