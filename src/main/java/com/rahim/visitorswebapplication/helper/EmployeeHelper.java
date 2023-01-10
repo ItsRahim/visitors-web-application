46cf5fb4a27e81ba094cb7b63647d25720c7b4e8
ItsRahim <rahim1605@gmail.com>
2023-01-10 10:30:48 +0000

    implemented method to check if email exists before creating entity

--- src/main/java/com/rahim/visitorswebapplication/helper/EmployeeHelper.java
+++ src/main/java/com/rahim/visitorswebapplication/helper/EmployeeHelper.java
@@ -1,9 +1,22 @@
 package com.rahim.visitorswebapplication.helper;
 
+import com.rahim.visitorswebapplication.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+
 import java.text.MessageFormat;
 
+@RequiredArgsConstructor
 public class EmployeeHelper {
-    public static String emailFormatter(String firstName, String lastName) {
+    private final EmployeeRepository employeeRepository;
+    private String emailFormatter(String firstName, String lastName) {
         return MessageFormat.format("{0}.{1}@bupa.com", firstName, lastName).toLowerCase();
+    }
+
+    /*
+    TODO: Revise this approach for checking if email exists - only accounts for a pair of duplicates and not more
+     */
+    public String checkEmailExists(String firstName, String lastName) {
+        String email = emailFormatter(firstName, lastName);
+        return employeeRepository.existsByEmail(email) ? emailFormatter(firstName, lastName+1) : email;
     }
 }
