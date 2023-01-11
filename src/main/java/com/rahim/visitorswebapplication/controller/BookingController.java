125cbca85665b204e267f4c3ca45529b3d524018
ItsRahim <rahim1605@gmail.com>
2023-01-11 12:05:56 +0000

    initialised controller class for booking

--- src/main/java/com/rahim/visitorswebapplication/controller/BookingController.java
+++ src/main/java/com/rahim/visitorswebapplication/controller/BookingController.java
@@ -1,4 +1,13 @@
 package com.rahim.visitorswebapplication.controller;
 
+import com.rahim.visitorswebapplication.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/booking")
+@RequiredArgsConstructor
 public class BookingController {
+    private final BookingService bookingService;
 }
