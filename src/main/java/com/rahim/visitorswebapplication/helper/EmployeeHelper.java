252504427f0017acc077931de66eee8d860ba9a7
ItsRahim <rahim1605@gmail.com>
2023-01-11 12:07:35 +0000

    changed method to find email exists to work with new reposisotry method

--- src/main/java/com/rahim/visitorswebapplication/helper/EmployeeHelper.java
+++ src/main/java/com/rahim/visitorswebapplication/helper/EmployeeHelper.java
@@ -1,22 +1,26 @@
 package com.rahim.visitorswebapplication.helper;
 
+import com.rahim.visitorswebapplication.model.Employee;
 import com.rahim.visitorswebapplication.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.text.MessageFormat;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class EmployeeHelper {
-//    private final EmployeeRepository employeeRepository;
-    public static String emailFormatter(String firstName, String lastName) {
+    private final EmployeeRepository employeeRepository;
+
+    private String emailFormatter(String firstName, String lastName) {
         return MessageFormat.format("{0}.{1}@bupa.com", firstName, lastName).toLowerCase();
     }
 
     /*
     TODO: Revise this approach for checking if email exists - only accounts for a pair of duplicates and not more
      */
-//    public String checkEmailExists(String firstName, String lastName) {
-//        String email = emailFormatter(firstName, lastName);
-//        return employeeRepository.existsByEmail(email) ? emailFormatter(firstName, lastName+1) : email;
-//    }
+    public String generateEmail(String firstName, String lastName) {
+        String email = emailFormatter(firstName, lastName);
+        Optional<Employee> employee = employeeRepository.findByEmail(email);
+        return employee.isPresent() ? emailFormatter(firstName, lastName + 1) : email;
+    }
 }
