c08733fed223451ec936f28d3f295c7a9c8e3bc0
ItsRahim <rahim1605@gmail.com>
2023-01-11 10:14:34 +0000

    added default method to find by id and query to delete by id

--- src/main/java/com/rahim/visitorswebapplication/repository/BookingRepository.java
+++ src/main/java/com/rahim/visitorswebapplication/repository/BookingRepository.java
@@ -1,9 +1,18 @@
 package com.rahim.visitorswebapplication.repository;
 
 import com.rahim.visitorswebapplication.model.Booking;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, String> {
+    default Booking findByIdOrError(String id) {
+        return findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+    @Modifying
+    @Query("DELETE FROM Booking b WHERE b.id = ?1")
+    void deleteByBookingId(String Id);
 }
