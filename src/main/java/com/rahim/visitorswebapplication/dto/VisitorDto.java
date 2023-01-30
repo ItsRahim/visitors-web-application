2bd8ecb4c2bc319cce91310ed4315a89ae3fe166
ItsRahim <rahim1605@gmail.com>
2023-01-30 14:18:26 +0000

    created visitor dto

---
+++ src/main/java/com/rahim/visitorswebapplication/dto/VisitorDto.java
@@ -0,0 +1,11 @@
+package com.rahim.visitorswebapplication.dto;
+
+import lombok.Data;
+
+@Data
+public class VisitorDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}
