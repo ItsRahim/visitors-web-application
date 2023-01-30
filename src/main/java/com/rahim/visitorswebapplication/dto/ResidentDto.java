844b3476978c2917ab8e18d5320ea5ab37922b05
ItsRahim <rahim1605@gmail.com>
2023-01-30 14:17:26 +0000

    created resident dto

---
+++ src/main/java/com/rahim/visitorswebapplication/dto/ResidentDto.java
@@ -0,0 +1,11 @@
+package com.rahim.visitorswebapplication.dto;
+
+import lombok.Data;
+
+@Data
+public class ResidentDto {
+    private String firstName;
+    private String lastName;
+    private String unit;
+    private int roomNumber;
+}
