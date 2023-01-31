7677fc58e80628c7770ef9337df4a0fb2c3fb9fc
ItsRahim <rahim1605@gmail.com>
2023-01-31 14:56:42 +0000

    updated method to take into account more than one duplicate email

--- src/main/java/com/rahim/visitorswebapplication/helper/EmployeeHelper.java
+++ src/main/java/com/rahim/visitorswebapplication/helper/EmployeeHelper.java
@@ -1,26 +1,20 @@
 package com.rahim.visitorswebapplication.helper;
 
-import com.rahim.visitorswebapplication.model.Employee;
 import com.rahim.visitorswebapplication.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 
-import java.text.MessageFormat;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 public class EmployeeHelper {
     private final EmployeeRepository employeeRepository;
 
-    private String emailFormatter(String firstName, String lastName) {
-        return MessageFormat.format("{0}.{1}@bupa.com", firstName, lastName).toLowerCase();
-    }
-
-    /*
-    TODO: Revise this approach for checking if email exists - only accounts for a pair of duplicates and not more
-     */
     public String generateEmail(String firstName, String lastName) {
-        String email = emailFormatter(firstName, lastName);
-        Optional<Employee> employee = employeeRepository.findByEmail(email);
-        return employee.isPresent() ? emailFormatter(firstName, lastName + 1) : email;
+        String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@bupa.com";
+        int counter = 1;
+        while (employeeRepository.findByEmail(email)) {
+            email = firstName.toLowerCase() + "." + lastName.toLowerCase() + counter + "@bupa.com";
+            counter++;
+        }
+        return email;
     }
 }
